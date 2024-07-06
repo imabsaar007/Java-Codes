7ce758d55ea416569a9dd20c22e339d296ea3675
Md Absaar <128820019+imabsaar007@users.noreply.github.com>
2024-07-06 08:39:42 +0000

    updated today

---
+++ java codes/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num1 = 24;
+        int num2 = 21;
+        int num3 = 11;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+
+    }
+}
--- java codes/hello.java
+++ java codes/hello.java
@@ -3,3 +3,5 @@ class HelloWorld {
         System.out.println("Hola Soy Leo Messi"); 
     }
 }
+// AddTwoNumber ----> Pascal Case Convention  (Used in Classes)
+// addTwoNumber -----> Camel Case Convention  (Used in Function)
