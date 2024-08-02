d3e838adfd55202b602199a851d350f4c95453b1
Md Absaar <128820019+imabsaar007@users.noreply.github.com>
2024-08-02 19:51:30 +0530

    Added few code object oriented programming based codes

---
+++ Dog.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Animal
+{
+    public void run()
+    {
+        System.out.println("I am running");
+    }
+}
+class Dog extends Animal
+{
+    public static void main(String[] args) {
+		Dog d = new Dog();
+		d.run();
+		
+	}
+}
+	
+
